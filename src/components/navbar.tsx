5f436b04ca1a2b55df3848da87fe3d1da34c819d
Rey Halsall <reyeduardohalsallquintero8@gmail.com>
2023-11-07 01:17:07 -0600

    Creating the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,17 @@
 function Navbar() {
 
     return (
-        <div>
+        <div className="bg-pink-400 flex justify-around align items-center p-2 border-b-black border-b-2">
+            <div className="w-[4rem] flex justify-center items-center">
+                <img src="/black_logo.svg" alt="shornel" />
+                <p className="ml-1 font-mono font-bold text-2xl">Shornel</p>
+            </div>
+            <div className="">
+                <a href="" className="mr-5">Home</a>
+                <a href="" className="mr-5">Our story</a>
+                <a href="" className="mr-5">Team</a>
+                <a href="">Projects</a>
+            </div>
         </div>
     )
 }
